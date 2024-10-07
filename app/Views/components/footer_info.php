d808362d949a9bb193c9580f299404754dcb4bf7
Ezk1e <tiratiraezekiel@gmail.com>
2024-10-07 22:08:02 +0800

    Refactor: Added footer_info component for Landing, About, and Info Pages

---
+++ app/Views/components/footer_info.php
@@ -0,0 +1,26 @@
+<!-- Footer -->
+<div class="darkblue pb-3">
+    <div class = "container text-center text-md-left mb-5">
+        <div class ="row card-footer border-white border-opacity-10">
+            <div class="mx-auto pt-5 col-md-4 col-lg-4 col-xl-4">
+                <h3 class = "fw-bold cyan">Cebu Pacific</h3>
+                <p>We are the leading airline in the Philippines, operating flights to over 60 domestic and international destinations across 14 countries, including Australia, China, Japan, Singapore and the United Arab Emirates.</p>
+            </div>
+
+            <div class="mx-auto pt-5 col-4 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+                <h3 class = "fw-bold cyan">Services</h3>
+                <p class = "center">Payment Options</p>
+                <p class = "center">Book Flights</p>
+                <p class = "center">Flight Status</p>
+                <p class = "center">Check-in</p>
+            </div>
+
+            <div class="mx-auto pt-5 col-8 col-sm-9 col-md-3 col-lg-3 col-xl-3">
+                <h3 class = "fw-bold cyan">Contacts</h3>
+                <p><i class="fa fa-home me-3" aria-hidden="true"></i>8006 Domestic Road, Pasay, Philippines 1301</p>
+                <p><i class="fa fa-envelope me-3" aria-hidden="true"></i>groupbookings@cebupacificair.com</p>
+                <p><i class="fa fa-phone me-3" aria-hidden="true"></i>+63 2-8702-0888</p>
+            </div>
+        </div>
+    </div>
+</div>
