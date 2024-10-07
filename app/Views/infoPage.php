f26c8f8c6e382ad0eb29bd5f60ffa793c6e09921
Ezk1e <tiratiraezekiel@gmail.com>
2024-10-07 21:37:39 +0800

    Refactor: Added header_nav component for Landing, About, and Info Pages

--- app/Views/infoPage.php
+++ app/Views/infoPage.php
@@ -11,33 +11,7 @@
     </head>
 
     <body>
-        <!-- Header -->
-        <nav class="navbar navbar-expand-sm navbar-dark navbar-custom">
-            <div class="container">
-                <a href="welcome_message.php" class="navbar-brand text-uppercase fs-5">
-                    <img src="" alt="Logo" width="70" class="d-inline-block align-middle">
-                    Cebu Pacific
-                </a>
-
-                <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target=".navbar-collapse">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="list1">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                            <a href="<?= base_url() ?>" class="nav-link">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="<?= base_url() . 'travelInformation'?>" class="nav-link">Travel Info</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="<?= base_url() . 'aboutUs' ?>" class="nav-link">About</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <?= $this->include('components/header_nav') ?>
 
         <!-- CODE HERE -->
         <h1>TRAVEL INFO PAGE</h1>
