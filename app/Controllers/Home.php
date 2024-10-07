bf044c56457ad1b3fe4419b3de4d56dca88c3d0d
Ezk1e <tiratiraezekiel@gmail.com>
2024-10-07 21:12:50 +0800

    Intialized Landing, About, Info Pages w/ Routes and Functions

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,19 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+
+    public function landing(): string
+    {
+        return view('landingPage');
+    }
+
+    public function about(): string
+    {
+        return view('aboutPage');
+    }
+
+    public function info(): string
+    {
+        return view('infoPage')
+    }
 }
